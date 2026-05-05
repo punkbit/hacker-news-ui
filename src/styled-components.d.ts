b5540788096232e5793b8f1f1199222dee2f4731
Helder Oliveira <heldrida@gmail.com>
2026-05-05 01:55:32 +0100

    fix: add react-router type declarations and extend styled-components types to fix compilation errors

--- src/styled-components.d.ts
+++ src/styled-components.d.ts
@@ -2,6 +2,12 @@ declare module 'styled-components' {
   import * as React from 'react';
   
   export interface DefaultTheme {
+    orange?: string;
+    vividTangelo?: string;
+    platinum?: string;
+    gainsBoro?: string;
+    onyx?: string;
+    sandyBrown?: string;
     [key: string]: any;
   }
   
@@ -22,26 +28,33 @@ declare module 'styled-components' {
     ...interpolations: any[]
   ): string;
   
-  interface StyledInterface {
-    <P = {}>(strings: TemplateStringsArray, ...interpolations: any[]): React.ComponentType<P>;
+  export interface StyledComponent<T> {
+    (strings: TemplateStringsArray, ...interpolations: any[]): React.ComponentType<T & { 
+      children?: React.ReactNode;
+      onClick?: () => void;
+      hide?: boolean;
+    }>;
   }
   
   const styled: {
-    div: StyledInterface;
-    span: StyledInterface;
-    button: StyledInterface;
-    a: StyledInterface;
-    section: StyledInterface;
-    article: StyledInterface;
-    header: StyledInterface;
-    footer: StyledInterface;
-    main: StyledInterface;
-    nav: StyledInterface;
-    h1: StyledInterface;
-    h2: StyledInterface;
-    h3: StyledInterface;
-    p: StyledInterface;
-    [tag: string]: StyledInterface;
+    div: StyledComponent<any>;
+    span: StyledComponent<any>;
+    button: StyledComponent<any>;
+    a: StyledComponent<any>;
+    section: StyledComponent<any>;
+    article: StyledComponent<any>;
+    header: StyledComponent<any>;
+    footer: StyledComponent<any>;
+    main: StyledComponent<any>;
+    nav: StyledComponent<any>;
+    h1: StyledComponent<any>;
+    h2: StyledComponent<any>;
+    h3: StyledComponent<any>;
+    p: StyledComponent<any>;
+    img: StyledComponent<any>;
+    svg: StyledComponent<any>;
+    path: StyledComponent<any>;
+    [tag: string]: StyledComponent<any>;
   };
   
   export default styled;
